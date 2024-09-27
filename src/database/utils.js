b491bab57f22e4355c799779c7ea285cbc096545
Jav1er <javier12bg@gmail.com>
2024-09-27 00:33:50 -0600

    Se implemento la logica para la conexion a la base de datos de la aplicacion y ademas algunas de las funcionabilidades para actualizar esto.

---
+++ src/database/utils.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const mysql = require('mysql');
+
+const saveToDb = (db) => {
+    fs.writeFileSync("./src/database/db.json", JSON.stringify(db, null, 2), {
+        encoding: "utf8",
+    });
+}
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: process.env.USER_NAME,
+  password: process.env.USER_PASS,
+  database: process.env.USER_DB
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected DataBase!');
+});
+
+// connection.end((err) => {
+//     if (err) throw err;
+//     console.log('Connection closed.');
+// });
+
+module.exports = { connection };
